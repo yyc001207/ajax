4176fa074cd127db89c4b5293b143a739fd2843d
yyc <1536376956@qq.com>
2022-10-20 12:17:05 +0800

    master

--- 代码/2.原生AJAX/server.js
+++ 代码/2.原生AJAX/server.js
@@ -7,9 +7,11 @@ const app = express()
 //创建路由规则
 //request封装请求报文
 //response封装响应报文
-app.get('/', (request, response) => {
-    //设置响应
-    response.send('HELLO EXPRESS')
+app.get('/server', (request, response) => {
+    //设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    //设置响应体
+    response.send('HELLO AJAX')
 })
 
 //监听端口启动服务
